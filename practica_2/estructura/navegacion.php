7333574e460d5895f68281ec26425204e8ffdeee
fatherboard <rapiteu@hotmail.com>
2020-03-18 11:38:47 +0100

    Añadido barra de navegación

---
+++ practica_2/estructura/navegacion.php
@@ -0,0 +1,8 @@
+<div class="navbar">
+        <a href="index.html?page=perfil">Inicio</a>
+        <a href="index.html?page=proyectos">Detalles</a>
+        <a href="index.html?page=foro">Equipo</a>
+        <a href="index.html?page=mensajes">Bocetos</a>
+        <a href="index.html?page=herramientas">Planificación</a>
+   
+</div>   
