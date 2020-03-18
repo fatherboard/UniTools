952bb09d80d9c59bbd827edebb202178a5232575
fatherboard <rapiteu@hotmail.com>
2020-03-18 15:28:44 +0100

    Estructura de la página hecha

--- practica_2/estructura/cabecera.php
+++ practica_2/estructura/cabecera.php
@@ -1,11 +1,3 @@
-<head>
-     
-    <link rel="stylesheet" type="text/css" href="../css/hoja.css">
-    <title>INDEX</title>
-    <meta charset="UTF-8">
-     
-</head>
-
 <div class="header">
     <a href="../estructura/inicio.php"><img src="../img/UniTools.png" alt="UniTools" width="120"></a>
 </div>
--- practica_2/estructura/navegacion.php
+++ practica_2/estructura/navegacion.php
@@ -1,8 +1,33 @@
+<?php
+    if(!isset($_SESSION)) 
+    { 
+        session_start(); 
+    } 
+?>
+
 <div class="navbar">
-        <a href="index.html?page=perfil">Inicio</a>
-        <a href="index.html?page=proyectos">Detalles</a>
-        <a href="index.html?page=foro">Equipo</a>
-        <a href="index.html?page=mensajes">Bocetos</a>
-        <a href="index.html?page=herramientas">Planificación</a>
-   
+
+
+    <a href="index.php?page=perfil">Perfil</a>
+    <a href="index.php?page=proyectos">Proyectos</a>
+    <a href="index.php?page=foro">Foro</a>
+    <a href="index.php?page=mensajes">Mensajes</a>
+    <a href="index.php?page=herramientas">Herramientas</a>
+
+    <?php
+            
+            if(!isset($_SESSION["login"]) || !($_SESSION["login"])) 
+            {
+                echo"<a class=\"aDerecha\" href=\"index.php?page=login\">Login</a>";
+                echo"<a class=\"aDerecha\" href=\"index.php?page=registrar\">Registrar</a>";
+                
+            }
+            else if ($_SESSION["login"])
+            {
+                echo"<p>Bienvenido</p>";
+              
+            }
+
+    ?>
+
 </div>   
