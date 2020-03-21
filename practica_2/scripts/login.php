a1e123510e843fde173f1b88b80532f5d96f84fb
Carlos Bilbao <Zildj1an@users.noreply.github.com>
2020-03-21 12:57:09 +0100

    Esto sobra

--- practica_2/scripts/login.php
+++ practica_2/scripts/login.php
@@ -22,30 +22,3 @@
         <?php 
             include("../estructura/pie.php");
         ?>
-
-  
-
-  
-/*
-// TODO: Inicia la sesión
-
-if (isset($_REQUEST['login_admin'])) 
-{
-    $_SESSION['usuario'] = 'admin';
-}
-else if (isset($_REQUEST['login'])) 
-{
-    $_SESSION['usuario'] = 'usuario';
-}
-else if (isset($_REQUEST['logout'])) 
-{
-    unset($_SESSION['usuario']);
-    // TODO: Borra la sesión
-}
-else {
-    // Mensaje de error y return
-}
-
-// TODO: Checkear contraseña
-*/
-?>
