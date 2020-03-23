09ef51f47b8fb6e2665246e125d0682dc811ae0b
Carlos Bilbao <Zildj1an@users.noreply.github.com>
2020-03-23 17:54:45 +0100

    Archivo para futuro Mensajes

---
+++ practica_2/scripts/mensajes.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="../css/hoja.css" />
+        <meta charset="utf-8">
+        <title>Mensajes</title>
+    </head>
+
+    <body>
+
+        <!-- Principio de la estructura de la página (contenedor) -->
+        <div id="contenedor">
+
+            <!-- Principio del contenido/funcionalidad del perfil -->
+            <div id="contenido">
+                <?php 
+                    //introducir aquí el contenido de Mensajes
+                    echo"<h1>Testo de prueba</h1>";
+                ?>
+            </div>
+            <!-- Fin del contenido -->
+
+        </div> <!-- Fin del contenedor -->
+
+    </body>
+</html>
