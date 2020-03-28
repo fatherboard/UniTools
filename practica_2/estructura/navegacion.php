1da53ea5bd47a03c7e627769e374c6f7f9d9612b
Onlime <onlime_@hotmail.com>
2020-03-28 13:50:23 +0100

    Modded navegación
    R

--- practica_2/estructura/navegacion.php
+++ practica_2/estructura/navegacion.php
@@ -1,31 +1,27 @@
 
 <div class="navbar">
-
-    <div align='left|justify'>
-    <a href="index.php?page=perfil">Perfil</a>
-    <a href="index.php?page=proyectos">Proyectos</a>
-    <a href="index.php?page=foro">Foro</a>
-    <a href="index.php?page=mensajes">Mensajes</a>
-    <a href="index.php?page=herramientas">Herramientas</a>
-    </div>
-
     <?php
-            
             if(!isset($_SESSION["login"]) || !($_SESSION["login"])) 
             {
-                //echo"<div align='right|justify'>";
+                echo "<a href=\"index.php\">Inicio</a>";
+                //TODO añadir página con perfiles de la práctica_1
+                //echo "<a href=\"index.php?page=aboutus\">Sobre nosotros</a>";
+                echo "<a href=\"index.php?page=foro\">Foro</a>";
+                echo "<a href=\"index.php?page=herramientas\">Herramientas</a>";
+                
                 echo"<a href=\"index.php?page=login\">Login</a>";
                 echo"<a href=\"index.php?page=registrar\" >Registrar</a>";
-                //echo"</div>";
             }
             else if ($_SESSION["login"])
             {
-                echo"<div align='right|justify'>";
+                echo"<a href=\"index.php?page=perfil\" >". $_SESSION["username"] . " </a>";
+                echo "<a href=\"index.php?page=proyectos\">Proyectos</a>";
+                echo "<a href=\"index.php?page=foro\">Foro</a>";
+                echo "<a href=\"index.php?page=mensajes\">Mensajes</a>";
+                echo "<a href=\"index.php?page=herramientas\">Herramientas</a>";
+
+                //TODO Derecha (css)
                 echo"<a href=\"index.php?page=logout\" >Logout</a>";
-                echo"<p> Bienvenido " . $_SESSION['username'] . "</p>";
-                echo"</div>";
             }
-
     ?>
-
 </div>
