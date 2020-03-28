93cf2b078627ffd8823a02c96fee38831c38ac39
Zildj1an <cbilbao@ucm.es>
2020-03-28 15:52:37 +0100

    Herramientas ya no refresca

--- practica_2/scripts/herramientas.php
+++ practica_2/scripts/herramientas.php
@@ -1,4 +1,4 @@
-        <div>   
+        <div>
              <h1>Herramientas</h1>
                 <p>UniTools pretende ayudar a los estudiantes de la Facultad en las asignaturas de programación.
                 Por ello, incluimos aquí herramientas de uso público y gratuito que pensamos pueden seros de
@@ -9,7 +9,7 @@
         <form style="text-align:center">
                <label for="phone" style="color:green"> (1) Introduzca su texto para darle formato:</label><br><br>
                <input type="integer" id="valor_d4" name="valor_d4" placeholder="Ejemplo"><br><br>
-               <button type="submit" onclick="texto();" onsubmit="return false">Convertir texto</button>
+               <button type="button" onclick="texto();">Convertir texto</button>
                 <p></p>
                 <canvas id="myCanvas" style = "width: 300px;height:100px;margin: 0px auto; border: 1px solid red;" style="border:1px solid #000000;">
                 </canvas>
@@ -19,18 +19,18 @@
         <form style="text-align: center">
             <label for="phone" style="color:green"> (2) Introduzca su texto para contar las palabras:</label><br><br>
             <input type="string" id="valor_d3" name="valor_d3" placeholder="fhritp"><br><br>
-            <button type="submit" onclick="count();" onsubmit="return false">Contar palabras</button>
+            <button type="button" onclick="count();" onsubmit="return false">Contar palabras</button>
         </form>
 	</div>
 	<div class = "split right">
         <form style="text-align: center">
             <label for="phone" style="color:green"> (3) Introduzca el decimal a convertir a hexadecimal:</label><br><br>
             <input type="integer" id="valor_d" name="valor_d" placeholder="123"><br><br>
-            <button type="submit" onclick="convertir(1);" onsubmit="return false">Convertir a hexadecimal</button>
+            <button type="button" onclick="convertir(1);" onsubmit="return false">Convertir a hexadecimal</button>
                 <p></p>
             <label for="phone" style="color:green"> (4) Introduzca el decimal a convertir a binario:</label><br><br>
             <input type="integer" id="valor_d2" name="valor_d2" placeholder="610"><br><br>
-            <button type="submit" onclick="convertir(2);" onsubmit="return false">Convertir a binario</button>
+            <button type="button" onclick="convertir(2);" onsubmit="return false">Convertir a binario</button>
         </form>
 	</div>
 
@@ -46,7 +46,7 @@
                 $str = parseInt(document.getElementById('valor_d').value);
                 ctx.strokeText($str,30,70);
                 alert('Hecho!');
-        }
+	}
 
         function convertir(option) {
                 if (option == 1) {
@@ -78,4 +78,4 @@
                 $str = $str.split(' ').length;
                 alert('Numero de palabras = ' + $str);
         }
-        </script>   
+        </script>
