617cc43750f0dcc6ed0db18c67d6558dec272c29
fatherboard <rapiteu@hotmail.com>
2020-03-30 13:57:01 +0200

    Arreglado el login (hemos vuelto a la del commit e15bd8a

--- practica_2/scripts/procesarLogin.php
+++ practica_2/scripts/procesarLogin.php
@@ -7,36 +7,32 @@
     
     /*
     Para evitar que se introduzca código en las entradas de username y password:
-
     htmlspecialchars -> convierte caracteres especiales "en texto"
     trim -> elimina espacios en blanco de la izquierda o derecha 
     strip_tags -> elimina tags de HTML, XML y PHP
     */
     $_SESSION['access_error'] = '0';
-    $_SESSION['access_success'] = FALSE;
+    $_SESSION['username'];
     $username = htmlspecialchars(trim(strip_tags($_REQUEST["username"])));
     $password = htmlspecialchars(trim(strip_tags($_REQUEST["password"])));
-    $_SESSION['username'] = $username;
-    $_SESSION['password'] = $password;
-
+    echo "$username " . " " . "$password";
     require_once 'connectdb.php';
-
     $query = mysqli_query($conn, "SELECT * FROM user WHERE Nick = '$username' AND password = '$password'");
-
     if(!$query){ 
         // echo "Usuario no existe " . $nombre . " " . $password. " o hubo un error " . 
         echo mysqli_error($conn);
         // si la consulta falla es bueno evitar que el código se siga ejecutando
         exit;
     } 
-
     //validamos los datos introducidos en el login
     if($user = mysqli_fetch_assoc($query)) {
         $conn->close();
-        $_SESSION['access_success'] = TRUE;
-        header("Location:../estructura/index.php");
-    } else {
+        $_SESSION['login'] = '1';
+        $_SESSION['username'] = $username;
+        header("location:../estructura/index.php");
+    } 
+    else {
         $_SESSION['access_error'] = '1';
-        header("Location:../estructura/index.php");
-    }  
+        header("location: ../estructura/index.php?page=login");
+    }      
 ?>
