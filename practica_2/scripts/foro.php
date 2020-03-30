e9aba0dfcbe13522950a39b10b4e3844af011e9b
fer2807 <frg1998@gmail.com>
2020-03-30 12:27:40 +0200

    variable login success para el foro

--- practica_2/scripts/foro.php
+++ practica_2/scripts/foro.php
@@ -1,9 +1,44 @@
-<div class="foro">
 
-
-    <a href="index.php?page=perfil">Lista de Posts</a> 
-    <a href="index.php?page=perfil">Crear Post</a>
 
    
 
-</div>   
+
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="../css/hoja.css" />
+        <meta charset="utf-8">
+        <title>Inicio</title>
+    </head>
+
+    <body>
+
+        <!-- Principio de la estructura de la página (contenedor) -->
+        <div id="contenedor">
+
+            <?php 
+                 require("../estructura/cabecera.php") ;
+                 require("../estructura/menu.php") ;
+            ?>
+
+           <!-- Principio del contenido/funcionalidad de procesar login -->
+           <div class="foro">
+           
+                <a href="index.php?page=perfil">Lista de Posts</a> 
+                <a href="index.php?page=perfil">Crear Post</a>
+
+   
+
+            </div>
+            <!-- Fin del contenido -->
+
+            <?php 
+                include("../estructura/pie.php");
+            ?>
+
+        </div> <!-- Fin del contenedor -->
+
+    </body>
+</html>
