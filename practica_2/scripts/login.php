8deef9c0ec0d929f5e7f06ef646b889565e16482
Carlos Bilbao <Zildj1an@users.noreply.github.com>
2020-03-31 17:57:31 +0200

    dj Carlos "placeholder en login" 2007 mp3

--- practica_2/scripts/login.php
+++ practica_2/scripts/login.php
@@ -2,9 +2,9 @@
            <form name="login" method="post" action = "../scripts/procesarLogin.php">
 
                 <table><tr><td>
-                    Username: </td> <td><input type ="text" name="username" required> </td></tr>
+                    Username: </td> <td><input type ="text" name="username" placeholder='Nombre usuario' required> </td></tr>
                     <tr><td>
-                    Password: </td> <td><input type="password" name = "password" required></td></tr>
+                    Password: </td> <td><input type="password" name = "password" placeholder='contraseña' required></td></tr>
                 </table>
                     <input type="submit" value = "Enviar">
 
