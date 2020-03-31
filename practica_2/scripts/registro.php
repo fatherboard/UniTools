7ddafa6804295c698bd1129cfc9475599e2653d8
Carlos Bilbao <Zildj1an@users.noreply.github.com>
2020-03-31 17:56:49 +0200

    dj Carlos "mejorar registro" mp3

--- practica_2/scripts/registro.php
+++ practica_2/scripts/registro.php
@@ -1,11 +1,11 @@
 
 <form name="register" method="post" action = "../scripts/procesarRegistro.php">
     <table><tr><td>
-    Username: </td> <td><input type ="text" name="username" required> </td></tr>
+    Username: </td> <td><input type ="text" name="username" placeholder='Username' required> </td></tr>
     <tr><td>
-    Email: </td> <td><input type ="text" name="email" required> </td></tr>
+    Email: </td> <td><input type ="text" name="email" placeholder='correo' required> </td></tr>
     <tr><td>
-    Password: </td> <td><input type="password" name = "password" required></td></tr>
+    Password: </td> <td><input type="password" name = "password" placeholder='contraseña' required></td></tr>
     </table>
     <input class="send-buttons" type="submit" value = "Enviar">
 </form>
