d84377d037d3ad69150a86159b255ad4d523a82c
Onlime <onlime_@hotmail.com>
2020-04-03 02:00:32 +0200

    perfil v0.3

--- practica_2/scripts/perfil.php
+++ practica_2/scripts/perfil.php
@@ -1,27 +1,23 @@
 
-<p> user id: <?php echo $_SESSION['userId'] ?></p> <br>;
-<p> user name: <?php $_SESSION["username"] ?></p>;
+<p> user id: <?php echo $_SESSION['userId'] ?></p> <br>
+<p> user name: <?php $_SESSION["username"] ?></p>
 
 <?php
-/* ?>
-
- include_once('/xampp/htdocs/UniTools/practica_2/dao/dao_user.php');
-    $user = new TOUser();
-    $dao_usuario = new DAOUsuario();
-
-
-
-$result = $dao_usuario->search_userId($_SESSION['user_id']);
-$result = $dao_usuario->search_username($username); 
-
-echo $_SESSION['user_id'];
-echo "user mail" . $user_data->get_email();
-
+include_once('/xampp/htdocs/UniTools/practica_2/dao/dao_user.php');
+$username = '5';
+$user = new TOUser();
+$dao_usuario = new DAOUsuario();
+$userData = $dao_usuario->search_username($username);
+/*
+$userData = $dao_usuario->search_userId($_SESSION['userId']);
+?>
+*/
+?>
 <div class="row">
     <div class="card">
         <img src="../UniTools/practica_2/img/Default_user_icon.jpg" alt="foto_perfil" style="width:100%" align="left">
         <div class="p_text">
-            <h5><b> <?php $user_data->get_email() ?> </b></h5>
+            <h5><b> <?php echo"hey" ?> </b></h5>
         </div>
     </div> 
 
@@ -30,5 +26,3 @@ echo "user mail" . $user_data->get_email();
         <p> dfadsaf </p>
     </div>
 </div>
-*/
-?>
