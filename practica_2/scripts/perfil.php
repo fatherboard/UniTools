f55d13d872bf79dd18c45a1d473a642f0d8d5eaf
Onlime <onlime_@hotmail.com>
2020-04-03 04:32:58 +0200

    AYAYA - Perfil casi terminado(con css)

--- practica_2/scripts/perfil.php
+++ practica_2/scripts/perfil.php
@@ -1,5 +1,7 @@
 
-<p> user name: <?php echo $_SESSION['username'] ?></p>
+<!--
+    <p> user name: <?php //echo $_SESSION['username'] ?></p>
+-->
 
 <?php
 include_once('/xampp/htdocs/UniTools/practica_2/dao/dao_user.php');
@@ -7,17 +9,21 @@ $user = new TOUser();
 $dao_usuario = new DAOUsuario();
 $userData = $dao_usuario->search_username($_SESSION['username']);
 ?>
+<body id="per">
 
-<div class="row">
-    <div class="card">
-        <img src="../UniTools/practica_2/img/Default_user_icon.jpg" alt="foto_perfil" style="width:100%" align="left">
-        <div class="p_text">
-            <h5><b> <?php echo $userData->get_username() ?> </b></h5>
+    <div class="per_columnaIzq">
+        <div class= "per_card">
+            <img id="per_foto" alt="foto_perfil" src="/UniTools/practica_2/img/Default_user_icon.jpg" >  
+            <p id="p_username"><b> Nombre de ususario: <?php echo $userData->get_username() ?> </b></p>
         </div>
-    </div> 
-
-    <div class="p_data">
-        <p> E-mail: <?php echo $userData->get_email()?> <br>
-            Contraseña actual: <?php echo $userData->get_password()?> </p>
     </div>
-</div>
+    <div class="per_columnaDer">
+        <div class="per_card">
+                E-mail: <?php echo $userData->get_email()?> <br>
+        </div> 
+        <div class="per_card">
+            Contraseña actual: <?php echo $userData->get_password()?> 
+        </div>
+    </div>
+   
+  
