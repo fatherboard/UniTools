2dc16c25d85fdc28deee13d2b8d6197d27ac15f4
Onlime <onlime_@hotmail.com>
2020-04-03 06:34:51 +0200

    perfil v2.1

--- practica_2/scripts/perfil.php
+++ practica_2/scripts/perfil.php
@@ -20,11 +20,13 @@ $userData = $dao_usuario->search_username($_SESSION['username']);
     </div>
 </div>
 <div class="per_columnaDer">
-<div class="per_card">
+
+<!-- premium user -->
+    <div class="per_card">
             Usuario premium: 
             <?php 
             if($userData->get_premium())
-                echo " Sí, días restantes: 29.";
+                echo " Sí, días restantes 29.";
             else{
                 echo " No... ¡Hazte premium hoy mismo! ";
                 echo "<input type='submit' value = 'Premium' name='premium'";
@@ -32,21 +34,31 @@ $userData = $dao_usuario->search_username($_SESSION['username']);
             ?> <br>
 
     </div> <br>
+
+    <!-- current email -->
     <div class="per_card">
             E-mail: <?php echo $userData->get_email()?> <br>
     </div> <br>
+
+    <!-- new email -->
     <div class="per_card">
         <label for='email'> Actualizar correo: </label>
         <input type='text' name='email' />
-        <input type="submit" value = "Actualizar" name="ac_email">
+        <input type="submit" value = "Actualizar" name="ac_email"  onclick= location.href = scripts/procesarLogin.php>
     </div><br>
+
+    <!-- current password -->
     <div class="per_card">
         Contraseña actual: <?php echo $userData->get_password()?> 
     </div><br>
+
+    <!-- new password #1-->
     <div class="per_card">
         <label for='firstpw'> Nueva contraseña: </label>
         <input type='password' name='email' />
     </div><br>
+
+    <!-- new password #2-->
     <div class="per_card">
         <label for='secondpw'> Volver a introducir: </label>
         <input type='password' name='email' />
