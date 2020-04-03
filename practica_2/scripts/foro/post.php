80276b5ec334b924900e39cc06d61ff64df83d75
Carlos Bilbao <Zildj1an@users.noreply.github.com>
2020-04-03 15:27:46 +0200

    inicio post

--- practica_2/scripts/foro/post.php
+++ practica_2/scripts/foro/post.php
@@ -1,38 +1,26 @@
 <a class= "botonForo" href="index.php?page=nuevoPost">Nuevo Post</a>
-    <a class= "botonForo" href="index.php?page=temas">Elegir Tema</a>
+<a class= "botonForo" href="index.php?page=temas">Elegir Tema</a>
 
-    <?php
-        echo "<h1> titulo del post </h1>";
-        echo "<h3> contenido del post </h3>";
+<?php
+	echo "<h1> titulo del post </h1>";
+	echo "<h3> contenido del post </h3>";
 
+	$foro_data = new TOUpost();
+	$dao_post = new DAOpost();
+	$dao_usuario = new DAOUsuario();
 
-         /*$query = mysqli_query($conn, "SELECT * FROM forumposts ORDER BY id_post DESC");
-        while($res = mysqli_fetch_array($query)){
-            
-            $userQuery=$res['user'];
-            $userById = mysqli_query($conn, "SELECT * FROM user WHERE id_user=$userQuery");  
-            $resultUser = mysqli_fetch_array($userById);
-            
-            echo "<table class=\"posts\">";
-	        echo "<tbody>";
-		    echo "<tr>";
-			echo "<td>usuario"</td>";
-			echo "<td>comentario</td>";
-		    echo "</tr>";
-	        echo "</tbody>";
-            echo "</table>";
-            
-        } 
-        
-        $conn->close();
+	$userData = $dao_usuario->search_username($_SESSION['username']);
 
-        include o cuadro de texto in-situ para realizar un comentario directamente desde la pantalla del post
+	$columna = [
+		"user" => $userData.get_name(),
+		"title" => $_SESSION['username'],
+		"content" => $_SESSION['contenido'],
+		"categoria" => $_SESSION['category']
+	];
+	$foro_data->create_Post($columna);
+	$data_post->insertPost($foro_data);
 
-        echo "<textarea class=\"respuesta\" placeholder=\"comentario\"></textarea>;
-
-        
-
-        */
-
-        
-    ?>
+	$dao_usuario->disconnect();
+	$dato_post->disconnect();
+	$dao_post->disconnect();       
+?>
