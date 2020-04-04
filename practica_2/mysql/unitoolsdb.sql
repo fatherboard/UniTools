be8c3e1905337a77dc50ce4f882930a2150981a2
fatherboard <rapiteu@hotmail.com>
2020-04-04 19:43:31 +0200

    Nueva bbddddddddddddddddddddddd

--- practica_2/mysql/unitoolsdb.sql
+++ practica_2/mysql/unitoolsdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 04, 2020 at 04:08 PM
+-- Generation Time: Apr 04, 2020 at 07:43 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.4
 
@@ -32,13 +32,6 @@ CREATE TABLE `categories` (
   `cat_name` varchar(25) NOT NULL,
   `desc` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `categories`
---
-
-INSERT INTO `categories` (`id_cat`, `cat_name`, `desc`) VALUES
-(0, 'general', 'Posts generales');
 
 -- --------------------------------------------------------
 
@@ -71,10 +64,13 @@ CREATE TABLE `posts` (
 --
 
 INSERT INTO `posts` (`id_post`, `user`, `title`, `content`, `id_cat`) VALUES
-(3, 4, 'Holaaaaaaaaaa', 'Esto es un contenido to wapo nen', 0),
-(5, 4, 'jeje', 'caca', 0),
-(6, 5, 'cacaca', 'cacasds', 0),
-(7, 8, 'VIVA EL BETIS', 'Aquí estamos todos pa cantarte tu canción\r\nestamos apiñados como balas de cañón\r\ny es que no hay quien pueda con esta afición\r\ny aunque último estuvieras siempre te ven campeón.\r\n\r\n\r\nBeeeeeetis, Beeeeeetis, Beeeeeetis.\r\nAhora Betis, ahora, no dejes de atacar\r\nahora Betis, ahora porque el gol ya va a llegar.\r\nBeeeeeetis, Beeeeeetis, Beeeeeetis\r\n\r\n\r\nHay una leyenda que recorre el mundo entero,\r\nverde y blanco sus colores,\r\nblanco y verde es el sendero,\r\nluz en la mañana y en la noche quejío y quiebro.\r\n\r\n\r\nBetis musho Betis, en el mundo lo que más quiero.\r\nLuz en la mañana y en la noche quejío y quiebro.\r\nBetis musho Betis, en este mundo lo que más quiero.\r\n\r\n\r\nBeeeeeetis, Beeeeeetis, Beeeeeetis\r\nAhora Betis, ahora, no dejes de atacar,\r\nahora Betis ahora porque el gol ya va a llegar.\r\nBeeeeeetis, Beeeeeetis, Beeeeeetis, Beeeeeetis, Beeeeeetis', 0);
+(2, 4, 'No sé hacer esto, ayuda', 'toi to perdio chavales, no sé sumar 1 a una variable xd', 0),
+(3, 5, 'Holaaaaaaaaaa', 'Esto es un contenido to wapo nen', 0),
+(5, 6, 'prueba asssssa', '3', 0),
+(6, 6, 'prueba asssssa', '3', 0),
+(7, 6, '[TEMA SERIO] Mi nobia ma dejao', 'soy programador de fp', 0),
+(8, 8, 'sssssss', '123', 0),
+(9, 9, 'xdeeee', 'xd', 0);
 
 -- --------------------------------------------------------
 
@@ -100,16 +96,8 @@ CREATE TABLE `respuesta` (
   `id_respuesta` int(11) NOT NULL,
   `user` int(11) NOT NULL,
   `date` date NOT NULL,
-  `content` text NOT NULL,
-  `id_post` int(11) NOT NULL
+  `content` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `respuesta`
---
-
-INSERT INTO `respuesta` (`id_respuesta`, `user`, `date`, `content`, `id_post`) VALUES
-(2, 5, '2020-03-16', 'esto es una critica constructiva', 3);
 
 -- --------------------------------------------------------
 
@@ -118,7 +106,7 @@ INSERT INTO `respuesta` (`id_respuesta`, `user`, `date`, `content`, `id_post`) V
 --
 
 CREATE TABLE `user` (
-  `id_User` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
   `email` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
   `username` varchar(30) NOT NULL,
@@ -129,8 +117,12 @@ CREATE TABLE `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id_User`, `email`, `password`, `username`, `premium`) VALUES
-(8, '1@hola.com', '$2y$10$uEAprH4ZpdzzCgqfokus2OudBwsEGdObcX6uqX.lVWFQc1e5NYKPC', 'pt_esp', 0);
+INSERT INTO `user` (`id_user`, `email`, `password`, `username`, `premium`) VALUES
+(4, 'caca@caca.com', '1', 'hugo', 1),
+(5, 'l@l.l', '1', 'l', 1),
+(6, '1@hola.com', '$2y$10$7gUTaS4clDnZgsUv2RuLeeei7/qFHFB9UBjYtvfQOcCnHV4MNJSPG', 'man', 0),
+(8, '1@hola.com', '$2y$10$qaUhcW0ficbue3HzsihzfOHqKw1/aMCJhwcLNobu/GvCCRjak7eea', 'pt_esp', 0),
+(9, '1@hola.com', '$2y$10$1/Zm3fnVyIh7jeBFBsN48uUXuMKptC3zT6pKlT.xg0vzzW98LKF7q', 'rapiteu', 0);
 
 --
 -- Indexes for dumped tables
@@ -155,7 +147,7 @@ ALTER TABLE `logs`
 ALTER TABLE `posts`
   ADD PRIMARY KEY (`id_post`),
   ADD KEY `user` (`user`),
-  ADD KEY `id_cat` (`id_cat`) USING BTREE;
+  ADD KEY `category` (`id_cat`) USING BTREE;
 
 --
 -- Indexes for table `repository`
@@ -169,14 +161,13 @@ ALTER TABLE `repository`
 --
 ALTER TABLE `respuesta`
   ADD PRIMARY KEY (`id_respuesta`),
-  ADD KEY `user` (`user`),
-  ADD KEY `id_post` (`id_post`) USING BTREE;
+  ADD KEY `user` (`user`);
 
 --
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`id_User`);
+  ADD PRIMARY KEY (`id_user`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -186,7 +177,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id_cat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_cat` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `logs`
@@ -198,7 +189,7 @@ ALTER TABLE `logs`
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id_post` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_post` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `repository`
@@ -210,43 +201,47 @@ ALTER TABLE `repository`
 -- AUTO_INCREMENT for table `respuesta`
 --
 ALTER TABLE `respuesta`
-  MODIFY `id_respuesta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_respuesta` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_User` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Constraints for dumped tables
 --
 
 --
+-- Constraints for table `categories`
+--
+ALTER TABLE `categories`
+  ADD CONSTRAINT `categories_ibfk_1` FOREIGN KEY (`id_cat`) REFERENCES `posts` (`id_cat`) ON UPDATE CASCADE;
+
+--
 -- Constraints for table `logs`
 --
 ALTER TABLE `logs`
-  ADD CONSTRAINT `logs_ibfk_1` FOREIGN KEY (`user`) REFERENCES `user` (`id_User`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `logs_ibfk_1` FOREIGN KEY (`user`) REFERENCES `user` (`id_user`) ON UPDATE CASCADE;
 
 --
 -- Constraints for table `posts`
 --
 ALTER TABLE `posts`
-  ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`user`) REFERENCES `user` (`id_User`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `posts_ibfk_2` FOREIGN KEY (`id_cat`) REFERENCES `categories` (`id_cat`) ON DELETE NO ACTION ON UPDATE CASCADE;
+  ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`user`) REFERENCES `user` (`id_user`) ON UPDATE CASCADE;
 
 --
 -- Constraints for table `repository`
 --
 ALTER TABLE `repository`
-  ADD CONSTRAINT `repository_ibfk_1` FOREIGN KEY (`creator`) REFERENCES `user` (`id_User`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `repository_ibfk_1` FOREIGN KEY (`creator`) REFERENCES `user` (`id_user`) ON UPDATE CASCADE;
 
 --
 -- Constraints for table `respuesta`
 --
 ALTER TABLE `respuesta`
-  ADD CONSTRAINT `respuesta_ibfk_1` FOREIGN KEY (`user`) REFERENCES `user` (`id_User`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `respuesta_ibfk_2` FOREIGN KEY (`id_post`) REFERENCES `posts` (`id_post`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `respuesta_ibfk_1` FOREIGN KEY (`user`) REFERENCES `user` (`id_user`) ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
