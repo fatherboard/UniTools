b6a658ef9a0ae7316ea986c0ac2de6f5eff21466
Onlime <onlime_@hotmail.com>
2020-04-04 17:49:33 +0200

    Perfil Terminado

--- practica_2/scripts/perfil.php
+++ practica_2/scripts/perfil.php
@@ -1,3 +1,4 @@
+
 <?php
 include_once('dao/dao_user.php');
 
@@ -7,15 +8,12 @@ if(!isset($_SESSION))
 $user = new TOUser();
 $dao_usuario = new DAOUsuario();
 $userData = $dao_usuario->search_username($_SESSION['username']);
-//inicialización de variables
-
-$emailUpdate = $password1 = $password2 = $premiumUpdate = "";
-$emailErr = $passwordErr = $premiumErr = "";
 
 $username = $_SESSION['username'];
 $premium = $userData -> get_premium();
 $email = $userData -> get_email();
 $password = $userData -> get_password();
+
 ?>
 
 
@@ -32,7 +30,6 @@ $password = $userData -> get_password();
 <div class="per_columnaDer">
     
 <!-- premium user -->
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> 
     <div class="per_card">
             Usuario premium: 
             <?php 
@@ -40,93 +37,18 @@ $password = $userData -> get_password();
                 echo " Sí, días restantes 29.";
             else{
                 echo " No... ¡Hazte premium hoy mismo! ";
-                echo "<input type='submit' value = 'Premium' name='premium'";
             }
             ?> <br>
-
     </div> <br>
 
-    <!-- current email -->
+<!-- current email -->
     <div class="per_card">
             E-mail: <?php echo $email?> <br>
     </div> <br>
 
-    <!-- new email -->
-    <div class="per_card">        
-        Actualizar correo:
-        <input type="text" name="email">
-        <span class="error">* <?php echo $emailErr;?> </span>
-        <input type="button" value = "Actualizar" name="ac_email" onclick=acEmail()>
-    </div><br>
-
-    <!-- current password -->
+<!-- current password -->
     <div class="per_card">
         Contraseña actual (encriptada): <?php echo $password?> 
     </div><br>
-
-    <!-- new password #1-->
-    <div class="per_card">
-        Nueva contraseña:
-        <input type="password" name="password1">
-        <span class="error">* </span>
-    </div><br>
-
-    <!-- new password #2-->
-    <div class="per_card">
-        Volver a introducir:
-        <input type="password" name="password2" value="<?php echo $password2;?>">
-        <span class="error">* <?php echo $passwordErr;?></span>
-        <input type="button" value = "Actualizar" name="ac_password"  onclick=acPassword()>
-    </div>
 </form>
 </div>
-
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (empty($_POST["email"])) {
-        $emailErr = "Es necesario introducir un correo";
-    } else {
-        $email = test_input($_POST["email"]);
-
-        // check if e-mail address is well-formed
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $emailErr = "Introduzca un correo válio";
-        }
-    }
-
-    if (empty($_POST["password1"]) || empty($_POST["password2"])) {
-        $passwordErr = "Es necesario rellenar ambas contraseñas";
-        } 
-        else {
-        $password1 = test_input($_POST["password1"]);
-        $password2 = test_input($_POST["password2"]);
-
-        // check if e-mail address is well-formed
-        if ($password1 != $password2) {
-            $passwordErr = "Las contraseñas no concident";
-        }
-    }
-
-
-    function test_input($data) {
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
-    }
-
-
-}
-?>
-
-<script>
-function acEmail() {
-    <?php echo $dao_usuario->update_email($username, "a@hotmail.com")?>
-    $email = $emailUpdate;
-}
-
-function acPassword() {
-     <?php echo $dao_usuario->update_password($username, $password)?>
-     $password = $password1;
-}
-</script>
