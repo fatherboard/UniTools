4bbfe8ac9ed1558fb20a987c2623fecdfaf473f3
fatherboard <rapiteu@hotmail.com>
2020-04-04 15:57:14 +0200

    Añadido mi base de datos con alteraciones

--- practica_2/mysql/unitoolsdb.sql
+++ practica_2/mysql/unitoolsdb.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.1
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 02-04-2020 a las 18:20:46
--- Versión del servidor: 10.4.11-MariaDB
--- Versión de PHP: 7.4.2
+-- Host: 127.0.0.1
+-- Generation Time: Apr 04, 2020 at 03:56 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,37 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `unitoolsdb`
+-- Database: `unitoolsdb`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `categories`
---
-
-CREATE TABLE `categories` (
-  `id_cat` int(11) NOT NULL,
-  `cat_name` varchar(25) NOT NULL,
-  `desc` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `logs`
---
-
-CREATE TABLE `logs` (
-  `id_log` int(11) NOT NULL,
-  `user` int(11) NOT NULL,
-  `date` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `posts`
+-- Table structure for table `posts`
 --
 
 CREATE TABLE `posts` (
@@ -57,187 +32,51 @@ CREATE TABLE `posts` (
   `user` int(11) NOT NULL,
   `title` text CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
   `content` text CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
-  `categoria` int(11) NOT NULL
+  `id_cat` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Volcado de datos para la tabla `posts`
+-- Dumping data for table `posts`
 --
 
-INSERT INTO `posts` (`id_post`, `user`, `title`, `content`, `categoria`) VALUES
-(2, 4, 'No sé hacer esto, ayuda', 'toi to perdio chavales, no sé sumar 1 a una variable xd', 0),
-(3, 5, 'Holaaaaaaaaaa', 'Esto es un contenido to wapo nen', 0);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `repository`
---
-
-CREATE TABLE `repository` (
-  `id_rep` int(11) NOT NULL,
-  `description` text NOT NULL,
-  `creator` int(11) NOT NULL,
-  `private` tinyint(1) NOT NULL,
-  `repURL` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
+INSERT INTO `posts` (`id_post`, `user`, `title`, `content`, `id_cat`) VALUES
+(3, 4, 'Holaaaaaaaaaa', 'Esto es un contenido to wapo nen', 0),
+(5, 4, 'jeje', 'caca', 0),
+(6, 5, 'cacaca', 'cacasds', 0),
+(7, 8, 'VIVA EL BETIS', 'Aquí estamos todos pa cantarte tu canción\r\nestamos apiñados como balas de cañón\r\ny es que no hay quien pueda con esta afición\r\ny aunque último estuvieras siempre te ven campeón.\r\n\r\n\r\nBeeeeeetis, Beeeeeetis, Beeeeeetis.\r\nAhora Betis, ahora, no dejes de atacar\r\nahora Betis, ahora porque el gol ya va a llegar.\r\nBeeeeeetis, Beeeeeetis, Beeeeeetis\r\n\r\n\r\nHay una leyenda que recorre el mundo entero,\r\nverde y blanco sus colores,\r\nblanco y verde es el sendero,\r\nluz en la mañana y en la noche quejío y quiebro.\r\n\r\n\r\nBetis musho Betis, en el mundo lo que más quiero.\r\nLuz en la mañana y en la noche quejío y quiebro.\r\nBetis musho Betis, en este mundo lo que más quiero.\r\n\r\n\r\nBeeeeeetis, Beeeeeetis, Beeeeeetis\r\nAhora Betis, ahora, no dejes de atacar,\r\nahora Betis ahora porque el gol ya va a llegar.\r\nBeeeeeetis, Beeeeeetis, Beeeeeetis, Beeeeeetis, Beeeeeetis', 0);
 
 --
--- Estructura de tabla para la tabla `respuesta`
---
-
-CREATE TABLE `respuesta` (
-  `id_respuesta` int(11) NOT NULL,
-  `user` int(11) NOT NULL,
-  `date` date NOT NULL,
-  `cat_resp` int(11) NOT NULL,
-  `content` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `user`
---
-
-CREATE TABLE `user` (
-  `id_User` int(11) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `username` varchar(30) NOT NULL,
-  `premium` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Volcado de datos para la tabla `user`
---
-
-INSERT INTO `user` (`id_User`, `email`, `password`, `username`, `premium`) VALUES
-(4, 'caca@caca.com', '1', 'hugo', 1),
-(5, 'l@l.l', '1', 'l', 1);
-
---
--- Índices para tablas volcadas
+-- Indexes for dumped tables
 --
 
 --
--- Indices de la tabla `categories`
---
-ALTER TABLE `categories`
-  ADD PRIMARY KEY (`id_cat`);
-
---
--- Indices de la tabla `logs`
---
-ALTER TABLE `logs`
-  ADD PRIMARY KEY (`id_log`),
-  ADD KEY `user` (`user`);
-
---
--- Indices de la tabla `posts`
+-- Indexes for table `posts`
 --
 ALTER TABLE `posts`
   ADD PRIMARY KEY (`id_post`),
   ADD KEY `user` (`user`),
-  ADD KEY `category` (`categoria`);
+  ADD KEY `id_cat` (`id_cat`) USING BTREE;
 
 --
--- Indices de la tabla `repository`
---
-ALTER TABLE `repository`
-  ADD PRIMARY KEY (`id_rep`),
-  ADD KEY `creator` (`creator`);
-
---
--- Indices de la tabla `respuesta`
---
-ALTER TABLE `respuesta`
-  ADD PRIMARY KEY (`id_respuesta`),
-  ADD KEY `user` (`user`),
-  ADD KEY `cat_resp` (`cat_resp`);
-
---
--- Indices de la tabla `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id_User`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT de la tabla `categories`
---
-ALTER TABLE `categories`
-  MODIFY `id_cat` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `logs`
---
-ALTER TABLE `logs`
-  MODIFY `id_log` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `posts`
+-- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id_post` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_post` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- AUTO_INCREMENT de la tabla `repository`
---
-ALTER TABLE `repository`
-  MODIFY `id_rep` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `respuesta`
---
-ALTER TABLE `respuesta`
-  MODIFY `id_respuesta` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `user`
---
-ALTER TABLE `user`
-  MODIFY `id_User` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- Restricciones para tablas volcadas
+-- Constraints for dumped tables
 --
 
 --
--- Filtros para la tabla `categories`
---
-ALTER TABLE `categories`
-  ADD CONSTRAINT `categories_ibfk_1` FOREIGN KEY (`id_cat`) REFERENCES `posts` (`categoria`) ON UPDATE CASCADE;
-
---
--- Filtros para la tabla `logs`
---
-ALTER TABLE `logs`
-  ADD CONSTRAINT `logs_ibfk_1` FOREIGN KEY (`user`) REFERENCES `user` (`id_User`) ON UPDATE CASCADE;
-
---
--- Filtros para la tabla `posts`
+-- Constraints for table `posts`
 --
 ALTER TABLE `posts`
-  ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`user`) REFERENCES `user` (`id_User`) ON UPDATE CASCADE;
-
---
--- Filtros para la tabla `repository`
---
-ALTER TABLE `repository`
-  ADD CONSTRAINT `repository_ibfk_1` FOREIGN KEY (`creator`) REFERENCES `user` (`id_User`) ON UPDATE CASCADE;
-
---
--- Filtros para la tabla `respuesta`
---
-ALTER TABLE `respuesta`
-  ADD CONSTRAINT `respuesta_ibfk_1` FOREIGN KEY (`user`) REFERENCES `user` (`id_User`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `respuesta_ibfk_2` FOREIGN KEY (`cat_resp`) REFERENCES `categories` (`id_cat`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`user`) REFERENCES `user` (`id_User`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `posts_ibfk_2` FOREIGN KEY (`id_cat`) REFERENCES `categories` (`id_cat`) ON DELETE NO ACTION ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
