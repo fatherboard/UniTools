445f6c9e282e69c42a69bc5345219dc847c37ab2
Carlos Bilbao <Zildj1an@users.noreply.github.com>
2020-04-04 13:10:51 +0200

    Bonitado

--- practica_2/scripts/foro/foro.php
+++ practica_2/scripts/foro/foro.php
@@ -4,18 +4,15 @@
     <a class= "botonForo" href="index.php?page=nuevoPost">Nuevo Post</a>
     <a class= "botonForo" href="index.php?page=temas">Elegir Tema</a>
 
-    <?php
-        echo "hola esto está en construcción";    
+    <?php   
         include_once("dao/dao_user.php");
         include_once("dao/dao_post.php");
-        
-        echo getcwd();
+       
         $foro_data = new TOUpost();
         $dao_post = new DAOpost();
         $dao_user = new DAOUsuario();
         $res = $dao_post->show_all_data();
         
-    
         while(!empty($res)){
             $curr_post = array_shift($res);
             $user_id = $curr_post->get_user(); // id del usuario que ha posteado
