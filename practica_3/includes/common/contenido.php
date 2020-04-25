dea92143cd789ffe2d44faab39295df863273447
Cansekito <60875184+Cansekito@users.noreply.github.com>
2020-04-25 18:04:06 +0200

    Contenido is back bitcheeeees

---
+++ practica_3/includes/common/contenido.php
@@ -0,0 +1,28 @@
+
+<div class "contenido">
+    <div class "apartado1">
+        <center>
+            <a href="index.php">
+                <img src="img/UniTools.png" alt="UniTools" width="70">
+            </a>
+        </center>
+        <h1> Unitools</h1>
+      
+    </div>
+
+    <div class "apartado2">
+        <h2><em>Tu lugar favorito en la nube </h2> <h3> Sube - Almacena - Descarga</em> </h3> 
+        <h4><cite> Únete a nuestra comunidad y disfruta de todas sus ventajas y posibilidades</cite> </h4>
+    </div> 
+    
+    <div class "apartado3">
+        <p> Unitools es una plataforma creada el 24 de Febrero de 2020 por un pequeño grupo de estudiantes.
+            <br /> La principal función de Unitool es permitirte tanto subir, actualizar y descargar tus archivos personales
+            de tal manera que puedas accceder a ellos desde cualquier sitio y desde todos tus dispositivos.
+            <br /> Gracias a nuestra comunidad, podrás hacer uso de diversas herramientas, iniciar temas de conversación,
+            resolver tus dudas y responder a otros usuarios.   
+        </p>
+    </div>   
+
+        <p> Aquí estará el contenido público, en estos momentos la página se encuentra en construcción. </p>;
+</div>
