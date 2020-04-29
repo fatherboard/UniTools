654b3dda8e1deec5ce5d6b8c1d34fe881887df8a
Carlos Bilbao <bilbao@vt.edu>
2020-04-29 14:16:35 +0200

    Empezando proyectos

--- practica_3/nuevo_project.php
+++ practica_3/nuevo_project.php
@@ -1,1 +1,85 @@
-TODO
+<?php
+if (!isset($_SESSION)) {
+    session_start();
+}
+
+include_once("dao/dao_project.php");
+include_once("dao/dao_user.php");
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" type="text/css" href="css/hoja.css">
+    <title>INDEX</title>
+    <meta charset="UTF-8">
+</head>
+
+<body>
+
+    <div id="contenedor">
+
+        <?php
+        require("includes/common/cabecera.php");
+        require("includes/common/navegacion.php");
+        ?>
+
+        <div id="contenido">
+            <?php
+            if (isset($_SESSION['login'])) {
+                if ($_SESSION['login']) {
+
+                    if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+                        //Falta categoria
+                        echo '<form action="" method="post">
+            <p>Título: <input type="text" name="titulo" /></p>
+            <p>Contenido: <textarea class="inputPost" name="contenido" ></textarea></p>
+            <p>Lenguaje: <textarea class="inputPost" name="lenguaje"></textarea></p>
+	    <p>Privado: <checkbox class="inputPost" name="privado"></checkbox></p> 
+            <p><input type="submit" /></p>
+            </form>';
+                    } else {
+                        //the form has been posted, so save it
+
+                        $titulo = $_POST['titulo'];
+                        $contenido = $_POST['contenido'];
+			$lenguaje = $_POST['lenguaje'];
+			$privado = $_POST['privado'];
+
+                        $proj_data = new TOUproject();
+                        $dao_proj = new DAOproject();
+                        $dao_user = new DAOUsuario();
+                        $user_id = $dao_user->search_username($_SESSION['username'])->get_id();
+                        $new_proj = new TOUProject('', $user_id, $titulo, $contenido, $lenguaje, $privado);
+
+                        if (!$dao_proj->insert_Project($new_proj)) {
+                            echo "Error al insertar project";
+                        } else {
+                            $dao_user->disconnect();
+                            header("location:proyects.php");
+                        }
+
+                        $dao_user->disconnect();
+                    }
+                } else {
+                    echo "Necesitas estar loggeado para hacer eso";
+                }
+            } else {
+                echo "Necesitas estar loggeado para hacer eso";
+            }
+
+            ?>
+        </div>
+
+        <?php
+        //muerte temporal del footer
+        //require("includes/common/pie.php") ; 
+        ?>
+
+
+    </div> <!-- Fin del contenedor -->
+
+</body>
+
--- practica_3/project.php
+++ practica_3/project.php
@@ -1,1 +1,74 @@
-TODO
+<?php
+if (!isset($_SESSION)) {
+	session_start();
+}
+include_once("dao/dao_project.php");
+include_once("dao/dao_user.php");
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+	<link rel="stylesheet" type="text/css" href="css/hoja.css">
+	<title>INDEX</title>
+	<meta charset="UTF-8">
+</head>
+
+<body>
+
+	<div id="contenedor">
+
+		<?php
+		require("includes/common/cabecera.php");
+		require("includes/common/navegacion.php");
+		?>
+
+		<div id="contenido">
+			<?php
+
+			$dao_resp = new DAOrespuesta();
+			$proj_data = new TOUproject();
+			$dao_proj = new DAOproject();
+			$dao_user = new DAOUsuario();
+			$id = $_GET["id"];
+
+			$curr_proj = $dao_proj->search_project($id);
+			$proj_id = $curr_proj->get_id(); // id del project
+			$usuario = $dao_user->search_userId($proj_id);
+			$lenguaje = $curr_proj->get_lenguaje();
+			$title = $curr_proj->get_title();
+			$candado = $curr_proj->get_candado();
+			$contenido = $curr_proj->get_content();
+
+			$proj = $dao_proj->search_project($id); //id viene del get de contenido
+
+			if ($usuario == null) {
+				$username = "Usuario borrado";
+			} else {
+				$username = $usuario->get_username();
+			}
+
+			echo "<h1>" . $title . "</h1></br>";
+			echo "<h3> LENGUAJE: " . $lenguaje . "</h3></br>";
+			echo "<h3> CANDADO:  " . $candado . "</h3></br>";
+			echo "<h3>" . $contenido . "</h3></br>"; 
+			
+			$dao_user->disconnect();
+
+			echo "<button onclick=\"location.href='editar_proj.php?proj=" . $proj_id . "'\">Editar</button>"
+			?>
+
+		</div>
+
+		<?php
+
+		//muerte temporal del footer
+		//require("includes/common/pie.php") ; 
+		?>
+
+
+	</div> <!-- Fin del contenedor -->
+
+</body>
