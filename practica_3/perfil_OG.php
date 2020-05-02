afd5e16c1fb11e7a4d1813a9adce54ceb707279b
Onlime <32745148+0nlime@users.noreply.github.com>
2020-05-03 00:53:11 +0200

    L.css updates

--- practica_3/perfil_OG.php
+++ practica_3/perfil_OG.php
@@ -16,23 +16,41 @@ include_once('dao/dao_user.php');
 </head>
 
 <body>
-
+    
     <div class="contenedor">
 
-        <div class="a menu">
-        <p> PERFIL </p>
-        <p> HERRAMIENTAS </p>
-        <p> FORO </p>
-        <p> AJUSTES </p>
-        <?php //require("includes/common/navegacion.php");?>
+        <div class="side_menu">
+            <div class="boxM z">
+            <?php echo "<a href=\"index.php\">Inicio</a>"?>
+            </div>
+            
+                <div class="boxM b">
+            <?php echo "<a href=\"foro.php\">Foro</a>"?>
+            </div>
+
+                <div class="boxM c">
+            <?php echo "<a href=\"herramientas.php\">Herramientas</a>"?>
+            </div>
+
+                <div class="boxM d">
+            <?php echo "<a href=\"team.php\">Nosotros</a>"?>
+            </div>
+
+                <div class="boxM e">
+            <?php echo"<a href=\"login.php\">Login</a>"?>
+            </div>
+
+                <div class="boxM f">
+            <?php echo"<a href=\"registro.php\" >Registrar</a>"?>
+            </div>
         </div>
 
-        <div class="a cabecera">
+        <div class="cabecera">
         INICIO LOGIN LOGOUT
         <?php //require("includes/common/cabecera.php");?>
         </div>
 
-        <div class="a contenido">
+        <div class="contenido">
            Contraseña actual (encriptada): Hey
         </div>
 
