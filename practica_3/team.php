5641365f193da3265fcaa9f17467e6750b9297e4
Cansekito <60875184+Cansekito@users.noreply.github.com>
2020-05-03 14:37:23 +0200

    Cambios hojas css Contenido, Animación y Team

--- practica_3/team.php
+++ practica_3/team.php
@@ -3,81 +3,9 @@
 
 <head>
 <link rel="stylesheet" type="text/css" href="css/hoja.css">
-
+<link rel="stylesheet" type="text/css" href="css/team.css">
 
 <style> 
-@import url('https://fonts.googleapis.com/css?family=Allura|Josefin+Sans');
-
-*{
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-
-body{
-  background: darkslategrey;
-  font-family: 'Josefin Sans', sans-serif;
-}
-
-.wrapper{
-  margin-top: 50px;
-}
-
-.wrapper h1{
-  font-family: 'Josefin Sans';
-  font-size: 52px;
-  margin-bottom: 60px;
-  text-align: center;
-}
-
-.team{
-  display: flex;
-  justify-content: center;
-  width: auto;
-  text-align: center;
-  flex-wrap: wrap;
-}
-
-.team .team_member{
-  background: #fff;
-  margin: 5px;
-  margin-bottom: 50px;
-  width: 300px;
-  padding: 20px;
-  line-height: 20px;
-  color: #8e8b8b;  
-  position: relative;
-}
-
-.team .team_member h3{
-  color: orangered;
-  font-size: 26px;
-  margin-top: 50px;
-}
-
-.team .team_member p.role{
-  color: #ccc;
-  margin: 12px 0;
-  font-size: 12px;
-  text-transform: uppercase;
-}
-
-.team .team_member .team_img{
-  position: absolute;
-  top: -50px;
-  left: 50%;
-  transform: translateX(-50%);
-  width: 100px;
-  height: 100px;
-  border-radius: 50%;
-  background: #fff;
-}
-
-.team .team_member .team_img img{
-  width: 100px;
-  height: 100px;
-  padding: 5px;
-}
 
 </style>
 
