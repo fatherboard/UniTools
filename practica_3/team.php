5771611db2e33e0e677fbd06f797d5556e57156f
Cansekito <60875184+Cansekito@users.noreply.github.com>
2020-05-03 14:31:37 +0200

    Página Sobre Nosotros

--- practica_3/team.php
+++ practica_3/team.php
@@ -2,8 +2,8 @@
 <html>
 
 <head>
-<link rel="stylesheet" type="text/css" href="./practica_3/css/hoja.css">
-<link rel="stylesheet" type="text/css" href="css/tea">
+<link rel="stylesheet" type="text/css" href="css/hoja.css">
+
 
 <style> 
 @import url('https://fonts.googleapis.com/css?family=Allura|Josefin+Sans');
@@ -15,7 +15,7 @@
 }
 
 body{
-  background: grey;
+  background: darkslategrey;
   font-family: 'Josefin Sans', sans-serif;
 }
 
@@ -24,7 +24,7 @@ body{
 }
 
 .wrapper h1{
-  font-family: 'Allura', cursive;
+  font-family: 'Josefin Sans';
   font-size: 52px;
   margin-bottom: 60px;
   text-align: center;
@@ -93,11 +93,11 @@ body{
         ?>
 
         <div class="wrapper">
-        <h1>Nuestro Equipo</h1>
+        <h1>El Team</h1> </br>
         <div class="team">
             <div class="team_member">
             <div class="team_img">
-                <img src="./practica_3/img/fotosTeam/depp.jpg" alt="Team_image">
+                <img src="img/fotosTeam/hugo.png" alt="Team_image">
             </div>
             <h3>Hugo Martins</h3>
             <p class="role">Linux Master</p>
@@ -105,14 +105,14 @@ body{
             </div>
             <div class="team_member">
             <div class="team_img">
-                <img src="./practica_3/img/fotosTeam/momoa.jpg" alt="Team_image">
+                <img src="img/fotosTeam/cansek.png" alt="Team_image">
             </div>
             <h3>Daniel Canseco</h3>
             <p class="role">Css noobi</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est quaerat tempora, voluptatum quas facere dolorum aut cumque nihil nulla harum nemo distinctio quam blanditiis dignissimos.</p></div>
             <div class="team_member">
             <div class="team_img">
-                <img src="./practica_3/img/fotosTeam/jobs.jpg" alt="Team_image">
+                <img src="img/fotosTeam/fer.png" alt="Team_image">
             </div>
             <h3>Fernando Ruiz</h3>
             <p class="role">Steve Job's Son</p>
@@ -120,7 +120,7 @@ body{
             </div>
             <div class="team_member">
             <div class="team_img">
-                <img src="./practica_3/img/fotosTeam/cepeda.jpg" alt="Team_image">
+                <img src="img/fotosTeam/cepeda.png" alt="Team_image">
             </div>
             <h3>Luis Cepeda</h3>
             <p class="role">Anonimous singer</p>
@@ -128,7 +128,7 @@ body{
             </div>
             <div class="team_member">
             <div class="team_img">
-                <img src="./practica_3/img/fotosTeam/wozniak.jpg" alt="Team_image">
+                <img src="img/fotosTeam/carlos.png" alt="Team_image">
             </div>
             <h3>Carlos Bilbao</h3>
             <p class="role">Black hat</p>
@@ -136,7 +136,7 @@ body{
             </div>
             <div class="team_member">
             <div class="team_img">
-                <img src="./practica_3/img/fotosTeam/bruno.jpg" alt="Team_image">
+                <img src="img/fotosTeam/bruno.png" alt="Team_image">
             </div>
             <h3>Bruno</h3>
             <p class="role">Your worst nightmare</p>
