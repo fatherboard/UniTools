31b0f1973c772c93cd73156c1d19e70907786b88
fer2807 <frg1998@gmail.com>
2020-05-05 20:48:13 +0200

    el puto premium va mejorando

--- practica_3/perfil.php
+++ practica_3/perfil.php
@@ -10,23 +10,24 @@ include_once('dao/dao_user.php');
 <html>
 
 <head>
-    <link rel="stylesheet" type="text/css" href="css/hoja.css">
-    <link rel="stylesheet" type="text/css" href="css/premium-popup.css">
-    <title>INDEX</title>
-    <meta charset="UTF-8">
+  <link rel="stylesheet" type="text/css" href="css/hoja.css">
+  <link rel="stylesheet" type="text/css" href="css/premium-popup.css">
+  <script defer src="script/popup.js"></script>
+  <title>INDEX</title>
+  <meta charset="UTF-8">
 </head>
 
 <body>
 
-    <div id="contenedor">
+  <div id="contenedor">
 
-        <?php
+    <?php
         require("includes/common/cabecera.php");
         require("includes/common/navegacion.php");
         ?>
 
-        <div id="contenido">
-            <?php
+    <div id="contenido">
+      <?php
 
             $user = new TOUser();
             $dao_usuario = new DAOUsuario();
@@ -41,64 +42,142 @@ include_once('dao/dao_user.php');
             ?>
 
 
-            <body id="per">
+      <body id="per">
 
-                <div class="per_columnaIzq">
-                    <div class="per_card">
-                        <div class="per_fotocard">
-                            <img id="per_foto" alt="foto_perfil" src="/UniTools/practica_2/img/Default_user_icon.jpg">
-                        </div>
-                        <p id="p_username"><b> Nombre de ususario: <?php echo $username ?> </b></p>
-                    </div>
-                </div>
-                <div class="per_columnaDer">
+        <div class="per_columnaIzq">
+          <div class="per_card">
+            <div class="per_fotocard">
+              <img id="per_foto" alt="foto_perfil" src="/UniTools/practica_2/img/Default_user_icon.jpg">
+            </div>
+            <p id="p_username"><b> Nombre de ususario: <?php echo $username ?> </b></p>
+          </div>
+        </div>
+        <div class="per_columnaDer">
 
-                    <!-- premium user -->
-                    <div class="per_card">
-                        Usuario premium:
-                        <?php
+          <!-- premium user -->
+          <div class="per_card">
+            Usuario premium:
+            <?php
                         if ($premium)
                             echo " Sí";
                         else {
                             echo " No... ¡Hazte premium hoy mismo! ";
                         }
-                        ?>
-                    <?php
-                        if($premium != 1){
-                           echo' <button class="open-button" onclick="popup()">Actualizar a Premium </button>';
-                        }
-                    ?>
-                    
-                    
-
-                    <script>
-                        function popup(){
-                            window.open('popup.php', 'popup', 'height=400, width=400, top=200, left=550,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no')
-                        }
-                    </script>
                         
-                         <br>
-                    </div> <br>
 
-                    <!-- current email -->
-                    <div class="per_card">
-                        E-mail: <?php echo $email ?> <br>
-                    </div> <br>
 
-                    <!-- current password -->
-                    <div class="per_card">
-                        Contraseña actual (encriptada): <?php echo $password ?>
-                    </div><br>
-                    </form>
-                </div>
+
+
+
+
+                
+                    if($premium == 0){
+                    
+
+            echo'<button data-modal-target="#modal" class="click-me">Actualizar a cuenta Premium </button>
+            <div class="modal" id="modal">
+              <div class="modal-header">
+                <div class="title"> Pásate al premium <br></div>
+                <button data-close-button class="close-buttonX"> &times;</button>
+              </div>
+
+              <div class="modal-body">
+                1-Consigue repositorios privados. <br>
+                2-Sin límite de tamaño de archivos. <br>
+                3-Di adiós a la publicidad <br>
+                4-Ocupa el lugar más alto de los rankings. <br>
+                5-Utiliza los mensajes de la plataforma. <br>
+                6-Recibe antes que nadie nuestras novedades. <br>
+                7-Forma parte de la gran familia de Unitools. <br>
+              </div>
+
+              <div class=modal-footer>
+                <form method="POST" action="">
+                  <button class="click-me" type="submit" name="confirm_update" onclick="actualizar()"> Suscribir
+                  </button>
+                </form>
+
+              </div>
+            </div>
+
+
+            <div id="overlay"></div>';
+            
+            }
+           else{
+                
+                echo '<button data-modal-target = "#modal" class="click-me">Dejar de ser Premium </button>
+                        <div class="modal" id="modal">
+                             <div class="modal-header">
+                                <div class="title"> ¿Seguro que quieres dejar de ser premium? <br></div>
+                                <button data-close-button class="close-buttonX"> &times;</button>    
+                            </div>
+
+                            <div class="modal-body">
+                            1-Consigue repositorios privados. <br>
+                            2-Sin límite de tamaño de archivos. <br>
+                            3-Di adiós a la publicidad <br>
+                            4-Ocupa el lugar más alto de los rankings. <br>
+                            5-Utiliza los mensajes de la plataforma. <br>
+                            6-Recibe antes que nadie nuestras novedades. <br>
+                            7-Forma parte de la gran familia de Unitools. <br>
+                          </div>
+
+
+                            <div class=modal-footer>   
+                                <form method="POST" action="">
+                                    <button class = "click-me"type="submit" name="confirm_update" onclick= "downgrade()"> Continuar </button>
+                                </form>    
+                                                    
+                            </div>
+                            <div  id="overlay"></div> 
+                        </div>';
+            }
+
+?>
+            
+            <br>
+          </div> <br>
+
+          <!-- current email -->
+          <div class="per_card">
+            E-mail: <?php echo $email ?> <br>
+          </div> <br>
+
+          <!-- current password -->
+          <div class="per_card">
+            Contraseña actual (encriptada): <?php echo $password ?>
+          </div><br>
+          </form>
         </div>
+    </div>
 
-        <?php
+    <?php
         //muerte temporal del footer
         //require("includes/common/pie.php") ; 
         ?>
 
 
-    </div> <!-- Fin del contenedor -->
+  </div> <!-- Fin del contenedor -->
 
 </body>
+
+
+
+<script>
+            function actualizar() {
+              
+              <?php $dao_usuario -> update_premium($username); ?>
+              self.close();
+              opener.location.href("perfil.php");
+            }
+            </script>
+
+            <script>
+            function downgrade() {
+              
+              <?php $dao_usuario -> downgrade_premium($username); ?>
+              self.close();
+              opener.location.href("perfil.php");
+            }
+            </script>
