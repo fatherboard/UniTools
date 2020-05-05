a3b3fc65b600ca626d5700162910f2183eae442e
Cansekito <60875184+Cansekito@users.noreply.github.com>
2020-05-05 14:30:59 +0200

    Enlaces pagina team y nuevo css

--- practica_3/team.php
+++ practica_3/team.php
@@ -4,71 +4,9 @@
 <head>
 <link rel="stylesheet" type="text/css" href="css/hoja.css">
 <link rel="stylesheet" type="text/css" href="css/team.css">
+<link rel="stylesheet" type="text/css" href="css/social.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<style>
-body { 
-color: #fff; 
-background: #262730 url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/93/noise-bg.png) repeat 0 0;
-}
 
-.social{
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
-}
-.social > a{
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
-}
-
-    .fa {
-  padding: 20px;
-  font-size: 30px;
-  width: 30px;
-  text-align: center;
-  text-decoration: none;
-  margin: 5px 2px;
-}
-
-.fa:hover {
-    opacity: 0.7;
-}
-
-.fa-facebook {
-  background: #3B5998;
-  color: white;
-}
-
-.fa-twitter {
-  background: #55ACEE;
-  color: white;
-}
-
-.fa-google {
-  background: #dd4b39;
-  color: white;
-}
-
-.fa-linkedin {
-  background: #007bb5;
-  color: white;
-}
-
-.fa-youtube {
-  background: #bb0000;
-  color: white;
-}
-
-.fa-instagram {
-  background: #125688;
-  color: white;
-}
-
-
-</style>
 
 
 </head>
@@ -136,12 +74,12 @@ background: #262730 url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/93/noise-bg
     </div>
 
     <div class="social">
-        <a href="#" class="fa fa-facebook"></a>
-        <a href="#" class="fa fa-twitter"></a>
-        <a href="#" class="fa fa-google"></a>
-        <a href="#" class="fa fa-linkedin"></a>
-        <a href="#" class="fa fa-youtube"></a>
-        <a href="#" class="fa fa-instagram"></a>
+        <a href="https://www.facebook.com/daniel.cansecomaldonado" class="fa fa-facebook"></a>
+        <a href="https://twitter.com/isMikeWazouski" class="fa fa-twitter"></a>
+        <a href="https://www.google.es" class="fa fa-google"></a>
+        <a href="https://es.linkedin.com/" class="fa fa-linkedin"></a>
+        <a href="https://www.youtube.com/user/Cansek98" class="fa fa-youtube"></a>
+        <a href="https://www.instagram.com/cansek98/" class="fa fa-instagram"></a>
     </div>
 	
 
