b1e32cbe9be7d289c598e5bb0014dd86015fb24f
Onlime <32745148+0nlime@users.noreply.github.com>
2020-05-10 00:57:21 +0200

    CSS - team DONE

--- practica_3/team.php
+++ practica_3/team.php
@@ -2,77 +2,96 @@
 <html>
 
 <head>
-<link rel="stylesheet" type="text/css" href="css/hoja.css">
-<link rel="stylesheet" type="text/css" href="css/team.css">
-<link rel="stylesheet" type="text/css" href="css/social.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <title>INDEX</title>
+    <meta charset="UTF-8">
+
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;1,300;1,100;0,200&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 
+    <link rel="stylesheet" type="text/css" href="css/hoja_OG.css">
+    <link rel="stylesheet" type="text/css" href="css/side_OG.css">
+    <link rel="stylesheet" type="text/css" href="css/cabecera_OG.css">
+    <link rel="stylesheet" type="text/css" href="css/content_OG.css">
+    
+   
+    <link rel="stylesheet" type="text/css" href="css/social.css">
 
 </head>
 
 <body>
+ <div class="contenedor">
 
-<div id="contenedor">
+<?php //class="side_menu"
+require("includes/common/navegacion_OG.php");?>
 
-    <?php
-    require("includes/common/cabecera.php");
-    require("includes/common/navegacion.php");
-    ?>
+<?php //class="cabecera"
+require("includes/common/cabecera_OG.php");?>
 
+<div class="contenido">
+   
     <div class="box">
-        <div class="wrapper">
-            <h1>El Team</h1> </br>
-            <div class="team">
-                <div class="team_member">
-                    <div class="team_img">
-                        <img src="img/fotosTeam/hugo.png" alt="Team_image">
-                    </div>
-                    <h3>Hugo Martins</h3>
-                    <p class="role">Linux Master</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est quaerat tempora, voluptatum quas facere dolorum aut cumque nihil nulla harum nemo distinctio quam blanditiis dignissimos.</p>
-                </div>
-                <div class="team_member">
+         <div class="fb-col box" id="team_titulo">
+		    <div class="t1 fb-row" >
+			    <h1>Nuestro equipo  </h1>
+            </div>
+        <div class="b1">
+            <p>Aquí encontrarás información sobre nuestros miembros y compenentes.</p>
+
+        </div>
+
+        
+        <div class="fb-row jc_space-between" id="team">
+            <div class="team_member">
                 <div class="team_img">
+                    <img src="img/fotosTeam/hugo.png" alt="Team_image">
+                </div>
+                <h3>Hugo Martins</h3>
+                <p class="role">Linux Master</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est quaerat tempora, voluptatum quas facere dolorum aut cumque nihil nulla harum nemo distinctio quam blanditiis dignissimos.</p>
+            </div>
+            <div class="team_member">
+                 <div class="team_img">
                     <img src="img/fotosTeam/cansek.png" alt="Team_image">
-                </div>
-                <h3>Daniel Canseco</h3>
-                <p class="role">Css noobi</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est quaerat tempora, voluptatum quas facere dolorum aut cumque nihil nulla harum nemo distinctio quam blanditiis dignissimos.</p></div>
-                <div class="team_member">
-                <div class="team_img">
-                    <img src="img/fotosTeam/fer.png" alt="Team_image">
-                </div>
-                <h3>Fernando Ruiz</h3>
+                 </div>
+                 <h3>Daniel Canseco</h3>
+                 <p class="role">Css noobi</p>
+                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est quaerat tempora, voluptatum quas facere dolorum aut cumque nihil nulla harum nemo distinctio quam blanditiis dignissimos.</p></div>
+            <div class="team_member">
+                 <div class="team_img">
+                     <img src="img/fotosTeam/fer.png" alt="Team_image">
+                    </div>
+                 <h3>Fernando Ruiz</h3>
                 <p class="role">Steve Job's Son</p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est quaerat tempora, voluptatum quas facere dolorum aut cumque nihil nulla harum nemo distinctio quam blanditiis dignissimos.</p>
-                </div>
-                <div class="team_member">
-                <div class="team_img">
-                    <img src="img/fotosTeam/cepeda.png" alt="Team_image">
-                </div>
+            </div>
+            <div class="team_member">
+                 <div class="team_img">
+                      <img src="img/fotosTeam/cepeda.png" alt="Team_image">
+                 </div>
                 <h3>Luis Cepeda</h3>
                 <p class="role">Anonimous calvo</p>
                 <p>Futuro ingeniero informático ---</p>
-                </div>
-                <div class="team_member">
+            </div>
+            <div class="team_member">
                 <div class="team_img">
                     <img src="img/fotosTeam/carlos.png" alt="Team_image">
                 </div>
                 <h3>Carlos Bilbao</h3>
                 <p class="role">Black hat</p>
                 <p>"Disfrutando de la creación de esta página web!"</p>
-                </div>
-                <div class="team_member">
+            </div>
+            <div class="team_member">
                 <div class="team_img">
                     <img src="img/fotosTeam/bruno.png" alt="Team_image">
                 </div>
                 <h3>Bruno</h3>
                 <p class="role">Your worst nightmare</p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est quaerat tempora, voluptatum quas facere dolorum aut cumque nihil nulla harum nemo distinctio quam blanditiis dignissimos.</p>
-                </div>
             </div>
         </div>
+        
+    
     </div>
 
     <div class="social">
@@ -84,8 +103,10 @@
         <a href="https://www.instagram.com/cansek98/" class="fa fa-instagram"></a>
     </div>
 	
+    </div>
 
 </div>
+
 </body>
 
 </html>
