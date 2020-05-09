dee37a53d8fc176ebd2a7f4e11b80eca26889a8a
Onlime <32745148+0nlime@users.noreply.github.com>
2020-05-09 14:34:43 +0200

    CSS - Pr  almost done

--- practica_3/project.php
+++ practica_3/project.php
@@ -31,74 +31,76 @@ require("includes/common/navegacion_OG.php");?>
 <?php //class="cabecera"
 require("includes/common/cabecera_OG.php");?>
 
-<div class="contenido">
-			<?php
-			$proj_data = new TOUproject();
-			$dao_proj = new DAOproject();
-			$dao_user = new DAOUsuario();
-			$id = $_GET["id"];
+	<div class="contenido">
+				<?php
+				$proj_data = new TOUproject();
+				$dao_proj = new DAOproject();
+				$dao_user = new DAOUsuario();
+				$id = $_GET["id"];
 
-			$curr_proj = $dao_proj->search_project($id);
-			$proj_id = $curr_proj->get_id(); // id del project
-			$usuario = $dao_user->search_userId($proj_id);
-			$lenguaje = $curr_proj->get_lenguaje();
-			$title = $curr_proj->get_titulo();
-			$estrellas = $curr_proj->get_estrellas();
-			$contenido = $curr_proj->get_contenido();
-			$candado = $curr_proj->get_candado();
+				$curr_proj = $dao_proj->search_project($id);
+				$proj_id = $curr_proj->get_id(); // id del project
+				$usuario = $dao_user->search_userId($proj_id);
+				$lenguaje = $curr_proj->get_lenguaje();
+				$title = $curr_proj->get_titulo();
+				$estrellas = $curr_proj->get_estrellas();
+				$contenido = $curr_proj->get_contenido();
+				$candado = $curr_proj->get_candado();
 
-			if ($usuario == null) {
-				$username = "Usuario borrado";
-			} else {
-				$username = $usuario->get_username();
-			}
-			
-			if ($candado === true ){
-				$candado = "ON";
-			}
-		    else {
-				$candado = "OFF";
-			}
-			?>
-			<div class="fb-col box" id="prs_g">
-				<div class="t1 fb-row" >
-					<h1>Proyecto</h1>
-				</div>
-				<div class="b1">
-             <table id='t01' style='width:100%'>
-             "<tr>
-             <?php //echo "<th>ID del Proyecto</th>"; ?>
-             <th>Titulo</th>
-             <th>Usuario</th>
-             <th>Lenguaje</th>
-             <th>Candado</th>
-             <th>Valoracion</th>
-            </tr>
-            <tr>
-             <td> <?php echo$title 	  ?></td>
-			 <td> <?php echo$username ?></td>
-			 <td> <?php echo$lenguaje ?></td>
-			 <td> <?php echo$candado  ?></td>
-			 <td> <?php echo$estrellas ?> estrellas </td>
-			</tr>
-             </table>  
-             
-             <table id='t01' style='width:100%'>
-            <tr>
-            <th> CONTENIDO </th>
-			 </tr>
-			 <tr>
-			<td> <?php echo $contenido ?> </td>
-			 </tr>
-             </table>
-     
-			$dao_user->disconnect();
+				if ($usuario == null) {
+					$username = "Usuario borrado";
+				} else {
+					$username = $usuario->get_username();
+				}
+				
+				if ($candado === true ){
+					$candado = "ON";
+				}
+				else {
+					$candado = "OFF";
+				}
+				?>
+				<div class="fb-col box" id="prs_g">
+					<div class="t1 fb-row" >
+						<h1>Proyecto</h1>
+					</div>
+					<div class="b1">
+						<table id='t01' style='width:100%'>
+						<tr>
+						<?php //echo "<th>ID del Proyecto</th>"; ?>
+							<th>Titulo</th>
+							<th>Usuario</th>
+							<th>Lenguaje</th>
+							<th>Candado</th>
+							<th>Valoracion</th>
+						</tr>
 
-			echo "<p></p><button onclick=\"location.href='editar_proj.php?proj=" . $proj_id . "'\">Editar</button>"
-			?>
+						<tr>
+							<td> <?php echo$title 	  ?></td>
+							<td> <?php echo$username ?></td>
+							<td> <?php echo$lenguaje ?></td>
+							<td> <?php echo$candado  ?></td>
+							<td> <?php echo$estrellas ?> estrellas </td>
+						</tr>
+						</table>  
+						
+						<table id='t01' style='width:100%'>
+						<tr>
+						<th> CONTENIDO </th>
+						</tr>
+						<tr>
+						<td> <?php echo $contenido ?> </td>
+						</tr>
+						</table>
+					</div>
+					
+				<?php
+				$dao_user->disconnect();?>
 
-		</div> 
+				<p></p><button onclick="location.href='editar_proj.php?proj= <?php echo $proj_id ?> ">Editar</button>
 
-	</div> <!-- Fin del contenedor -->
+				</div> 
+	</div>		
+</div> <!-- Fin del contenedor -->
 
 </body>
