b87006c1f750c86071dfa8a138b1308bdf17a23a
Cansekito <60875184+Cansekito@users.noreply.github.com>
2020-05-10 02:03:58 +0200

    css

--- practica_3/includes/common/contenido.php
+++ practica_3/includes/common/contenido.php
@@ -2,27 +2,28 @@
 
 <html>
     <head>
-        
+
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;1,300;0,500&display=swap" rel="stylesheet">  
+    <link rel="stylesheet" type="text/css" href="css/hoja_OG.css">
+    <link rel="stylesheet" type="text/css" href="css/side_OG.css">
+    <link rel="stylesheet" type="text/css" href="css/cabecera_OG.css">
+   
     <link rel="stylesheet" type="text/css" href="css/content_OG.css">  
        <!-- <link rel="stylesheet" type="text/css" href="css/contenidoanim.css">  -->
-        
+    
              
     </head>    
 
-    <body>
-      
+    <body>      
 
-         <div class="contenido">
-
-       
+         <div id="index">       
         
-            <div class="apartado1">
-                <center>
+            <div id="apartado1" class="text-center">
+                
                     <a href="index.php">
                         <img src="img/UniTools.png" alt="UniTools" width="120">
-                    </a>
-                </center>
-                <h1><strong> UniTools </strong></h1>
+                    </a>            
+                 
             
             </div>
             <!--
@@ -35,17 +36,20 @@
             
                 </div>
                 -->
-            <div class="apartado2">
-                <h2><em> Tu lugar favorito en la nube </h2>          
-                                    
-                <h3> Sube - Almacena - Descarga </em></h3> 
-                <a href = "registro.php">
-                    <h4><i><ins> Únete a nuestra comunidad y disfruta de todas sus ventajas y posibilidades </ins></i></h4>
-                </a>
-            </div> 
 
+            <div class="field">
+
+                <div id="apartado2" class="text-center">
+                    <h2><em> Tu lugar favorito en la nube </h2>          
+                                        
+                    <h3> Sube - Almacena - Descarga </em></h3> 
+                    <a href = "registro.php">
+                        <h4><i><ins> Únete a nuestra comunidad y disfruta de todas sus ventajas y posibilidades </ins></i></h4>
+                    </a>
+                </div> 
+            </div>
             
-            <div class="apartado3">
+            <div id="apartado3" class="text-center">
                     <p> 
                         Unitools es una plataforma creada el 24 de Febrero de 2020 por un pequeño grupo de estudiantes. 
                     </p>
@@ -63,27 +67,27 @@
                 
             </div>   
 
-            <div class="apartado4">
-                <center>
+            <div id="apartado4"  class="text-center">
+               
                     <img src ="img/iconosubida.png" alt="sube"width="50">
                     <img src ="img/iconocarpeta.png" alt="guarda"width="50"> 
                     <img src ="img/iconodescarga.png" alt="descarga" width="50">                         
-                </center>
+               
             </div>
 
-            <div class="apartado5">
+            <div id="apartado5"  class="text-center">
                 <p><mark> Actualmente la página se encuentra en construcción. </mark></p>
             </div>
 
-            <div class="apartado6">
-                <center>
+            <div id="apartado6"  class="text-center">
+              
                    <!-- <img src ="img/facultad.jpg" alt="facu"width="700"> -->
                     <img src ="img/img_back/photo-of-person-holding-mobile-phone-3183153.jpg" alt="sube"width="700">
                     
-                </center>
+             
             </div>   
 
-            <div class="apartado7">
+            <div id="apartado7"  class="text-center">
                 <embed src="music/musicafondo3.mp3" autostarty="true" loop="true" volume="60" width="0" height="0">
             </div>  
             
