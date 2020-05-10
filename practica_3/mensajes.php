61f619b12f8045d988a2e7d6363288ad1f4c89d8
fer2807 <frg1998@gmail.com>
2020-05-10 12:33:52 +0200

    MENSAJES CSS DONE

--- practica_3/mensajes.php
+++ practica_3/mensajes.php
@@ -8,24 +8,34 @@ if (!isset($_SESSION)) {
 <html>
 
 <head>
-    <link rel="stylesheet" type="text/css" href="css/hoja.css">
-    <title>INDEX</title>
+        <title>INDEX</title>
     <meta charset="UTF-8">
+
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;1,300;1,100;0,200&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" type="text/css" href="css/hoja_OG.css">
+    <link rel="stylesheet" type="text/css" href="css/side_OG.css">
+    <link rel="stylesheet" type="text/css" href="css/cabecera_OG.css">
+    <link rel="stylesheet" type="text/css" href="css/content_OG.css">
 </head>
 
 <body>
+ <div class="contenedor">
 
-    <div id="contenedor">
+<?php //class="side_menu"
+require("includes/common/navegacion_OG.php");?>
 
-        <?php
-        require("includes/common/cabecera.php");
-        require("includes/common/navegacion.php");
-        ?>
+<?php //class="cabecera"
+require("includes/common/cabecera_OG.php");?>
 
-        <div id="contenido">
+<div class="contenido">
+<div class="fb-col box" id="pg_ms">
+    <div class="t1 fb-row" >
             <?php
             echo "<h1>EN CONSTRUCCIÓN</h1>";
-            ?>
+            ?>  
+            </div>
+</div>
         </div>
 
         <?php
