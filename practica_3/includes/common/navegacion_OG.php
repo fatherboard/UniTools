2f9f93659ac59a71fa8c17c4866c13f0a15142be
fatherboard <rapiteu@hotmail.com>
2020-05-11 15:33:44 +0200

    Nombres respuesta foro arreglado

--- practica_3/includes/common/navegacion_OG.php
+++ practica_3/includes/common/navegacion_OG.php
@@ -5,100 +5,84 @@
 <ul class="side_menu margin-x0 margin-y0 color">
 
     <a class="side_unitools d-flex justify-content-center align-items-center margin" href=index.php>
-        <div class="side_unitools_texto c-w">
-            UniTools
-        </div>
+    <div class= "side_unitools_texto c-w">
+        UniTools
+    </div>
     </a>
 
     <hr class="side_divH">
 
     <li class="sideop color">
         <a href=index.php>
-            <i class="fas fa-house-user"></i>
-            <span>Inicio</span>
+        <i class="fas fa-house-user"></i>
+        <span>Inicio</span>
         </a>
     </li>
 
     <hr class="side_divH">
-
+    
     <?php
-    if (!isset($_SESSION["login"]) || !($_SESSION["login"])) { ?>
+    if(!isset($_SESSION["login"]) || !($_SESSION["login"])) 
+    { ?>
+        <div class="side_titulo">
+            Comunidad
+        </div>
+
+
+        <li class="sideop color">
+            <a href=herramientas.php>
+        <i class="fas fa-tools"></i>
+            <span>Herramientas</span>
+            </a>
+        </li>
+
+      <?php 
+    }
+    else if ($_SESSION["login"]) 
+    { ?>
+
+        <li class="sideop color">
+            <a href=perfil.php> 
+            <i class="far fa-id-badge"></i>
+            <span>Mi Pefil</span>
+            </a>
+        </li>
+
+        <hr class="side_divH">
+
         <div class="side_titulo">
             Comunidad
         </div>
 
         <li class="sideop color">
             <a href=proyectos.php>
-                <i class="fas fa-laptop-code"></i>
-                <span>Proyectos</span>
-            </a>
-        </li>
-
-        <li class="sideop color">
-            <a href=mensajes.php>
-                <i class="fas fa-comment-dots"></i>
-                <span>Mensajes</span>
-            </a>
-        </li>
-
-    <?php
-    } else if ($_SESSION["login"]) { ?>
-
-        <li class="sideop color">
-            <a href=perfil.php>
-                <i class="far fa-id-badge"></i>
-                <span>Mi Pefil</span>
-            </a>
-        </li>
-
-
-
-        <?php
-        if ($_SESSION['login']) {
-            echo '<hr class="side_divH">
-            <div class="side_titulo">
-            Comunidad
-            </div>
-            <li class="sideop color">
-            <a href=proyectos.php>
             <i class="fas fa-laptop-code"></i>
             <span>Proyectos</span>
             </a>
-            </li>
-            <li class="sideop color">
-            <a href=mensajes.php>
-            <i class="fas fa-comment-dots"></i>
-            <span>Mensajes</span>
-            </a>
-            </li>';
-        }
-        ?>
-
-
-
+        </li>
 
         <li class="sideop color">
             <a href=mensajes.php>
-                <i class="fas fa-comment-dots"></i>
-                <span>Mensajes</span>
+            <i class="fas fa-comment-dots"></i>
+            <span>Mensajes</span>
             </a>
         </li>
 
         <li class="sideop color">
             <a href=foro.php>
-                <i class="far fa-window-restore"></i>
-                <span>Foro</span>
+            <i class="far fa-window-restore"></i>
+            <span>Foro</span>
             </a>
         </li>
 
         <li class="sideop color">
             <a href=herramientas.php>
-                <i class="fas fa-tools"></i>
-                <span>Herramientas</span>
+        <i class="fas fa-tools"></i>
+            <span>Herramientas</span>
             </a>
         </li>
 
-    <?php
+      <?php 
     } ?>
 
     <hr class="side_divH">
@@ -109,8 +93,8 @@
 
     <li class="sideop color">
         <a href=team.php>
-            <i class="fas fa-user-astronaut"></i>
-            <span>Nuestro Equipo</span>
+        <i class="fas fa-user-astronaut"></i>
+        <span>Nuestro Equipo</span>
         </a>
     </li>
 
