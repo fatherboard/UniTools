16e9f390e1b51e8f375136313315843d7ea55ff7
Cansekito <60875184+Cansekito@users.noreply.github.com>
2020-05-24 13:26:06 +0200

    minicomentarios

--- practica_3/ajaxfile.php
+++ practica_3/ajaxfile.php
@@ -5,17 +5,21 @@ include "FormRegistro";
 if(isset($_POST['username'])){
    $username = $_POST['username'];
 
-   $query = "select count(*) as cntUser from users where username='".$username."'";
+   //Check if $username existe en la tabla users
 
+   $query = "select count(*) as cntUser from users where username='".$username."'";  
    $result = mysqli_query($con,$query);
-   $response = "<span style='color: green;'>Available.</span>";
+
+   //Crear una variable &responsey assignarla "Disponible"
+
+   $response = "<span style='color: green;'>Disponible.</span>";
    if(mysqli_num_rows($result)){
       $row = mysqli_fetch_array($result);
 
       $count = $row['cntUser'];
     
       if($count > 0){
-          $response = "<span style='color: red;'>Not Available.</span>";
+          $response = "<span style='color: red;'>No Disponible.</span>";
       }   
    }
 
--- practica_3/js/jquery_checkUser.js
+++ practica_3/js/jquery_checkUser.js
@@ -1,23 +1,23 @@
 $(document).ready(function(){
 
-    $("#username").keyup(function(){
+    $("#username").keyup(function(){  //Campo de user
  
-       var username = $(this).val().trim();
+       var username = $(this).val().trim(); //Leer rl valor y asignarlo a la variable username
  
-       if(username != ''){
+       if(username != ''){ //Ver si está vacio o no
  
-          $.ajax({
+          $.ajax({              // Si no está vacio enviar Ajax Post a ajaxfile.php
              url: 'ajaxfile.php',
              type: 'post',
-             data: {username: username},
+             data: {username: username},    // Pasar el username como data
              success: function(response){
  
-                 $('#uname_response').html(response);
+                 $('#uname_response').html(response); // si es correcto llamar a escribir Disponible
  
               }
           });
        }else{
-          $("#uname_response").html("");
+          $("#uname_response").html(""); //Si está vacio vaciar el texto de respuesta
        }
  
      });
