ace89437167eac2f347191dffdefa0db05581da7
fatherboard <rapiteu@hotmail.com>
2020-05-28 20:50:33 +0200

    Fix user

--- practica_3/project.php
+++ practica_3/project.php
@@ -58,7 +58,7 @@ include_once("dao/DAOestrellas.php");
 
 			if ($usuario == null) {
 				$username = "Usuario borrado";
-			} else {
+			} else if ($usuario instanceof TOUser){
 				$username = $usuario->get_username();
 			}
 
